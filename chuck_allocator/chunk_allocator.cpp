8b603a73c744ed6211c82fbfe25ae864d27289fe
Yuri Urentsov <yuri.urentsov@gmail.com>
2020-11-05 09:02:34 +0300

    test with copy constructor and copy operator

--- chuck_allocator/chunk_allocator.cpp
+++ chuck_allocator/chunk_allocator.cpp
@@ -1,9 +1,10 @@
+#include "chunk_allocator.h"
+
 #include <iostream>
 #include <vector>
 
-#include "main.h"
 
-using namespace task;
+using namespace std;
 
 // Tests
 class A {
@@ -22,35 +23,62 @@ public:
 };
 
 int main() {
-    task::MyAllocator<int> alloc;
-    size_t n1 = 10;
-    int *p1 = alloc.allocate(n1);
-    size_t n2 = 25;
-    int *p2 = alloc.allocate(n2);
-    size_t n3 = 10;
-    int *p3 = alloc.allocate(n3);
-    size_t n4 = 10;
-    int *p4 = alloc.allocate(n4);
-    alloc.deallocate(p1, n1);
-    alloc.deallocate(p3, n3);
-    alloc.deallocate(p2, n2);
-    alloc.deallocate(p4, n4);
+    {
+        task::ChunkAllocator<int> a1;
+        task::ChunkAllocator<int> a2(a1);
+        task::ChunkAllocator<int> a3 = a1;
+        a3 = a2;
+        task::ChunkAllocator<int> a4;
+        a3 = a4;
+    }
+    cout << "\n" << endl;
+    {
+        cout << "T = int" << endl;
+        cout << "declaration" << endl;
+        task::ChunkAllocator<int> alloc;
+        cout << "allocations" << endl;
+        size_t n1 = 10;
+        int *p1 = alloc.allocate(n1);
+        size_t n2 = 25;
+        int *p2 = alloc.allocate(n2);
+        size_t n3 = 10;
+        int *p3 = alloc.allocate(n3);
+        size_t n4 = 10;
+        int *p4 = alloc.allocate(n4);
+        cout << "deallocations" << endl;
+        alloc.deallocate(p1, n1);
+        alloc.deallocate(p3, n3);
+        alloc.deallocate(p2, n2);
+        alloc.deallocate(p4, n4);
+    }
 
-    std::cout << "\nclass A\n";
-    MyAllocator<A> a_aloc;
-    auto p = a_aloc.allocate(1);
-    a_aloc.construct(p, 1, 2);
-    a_aloc.destroy(p);
-    a_aloc.deallocate(p, 1);
-    p = a_aloc.allocate(1);
-    a_aloc.construct(p, 1, 2);
-    a_aloc.destroy(p);
-    a_aloc.deallocate(p, 1);
+    cout << "\n" << endl;
+    {
+        cout << "T = class A" << endl;
+        cout << "declaration" << endl;
+        task::ChunkAllocator<A> a_aloc;
+        cout << "allocation" << endl;
+        auto p = a_aloc.allocate(1);
+        a_aloc.construct(p, 1, 2);
+        a_aloc.destroy(p);
+        a_aloc.deallocate(p, 1);
+        cout << "allocations" << endl;
+        p = a_aloc.allocate(1);
+        a_aloc.construct(p, 1, 2);
+        a_aloc.destroy(p);
+        a_aloc.deallocate(p, 1);
+    }
+    cout << "\n" << endl;
+    {
+        cout << "T = vector" << endl;
+        task::ChunkAllocator<A> allocator;
+        std::vector<A, decltype(allocator)> vector1(5);
+        auto a1 = A(2, 4);
+        vector1.push_back(a1);
 
-    std::cout << "\nVectors\n";
-    std::vector<A, MyAllocator<A>> vector1(5);
-    auto a1 = A(2, 4);
-    vector1.emplace_back(a1);
+
+    }
+
 
 }
 
