8a6cca1757f9798b3fc5ff6997daa155b8bb4b8b
Yuri Urentsov <yuri.urentsov@gmail.com>
2020-11-09 11:37:43 +0300

    shared pointers added

--- smart_pointers/src/smart_pointers.h
+++ smart_pointers/src/smart_pointers.h
@@ -26,7 +26,7 @@ namespace task {
         /** Constructor from pointer:
         * initializing the stored pointer with p
         */
-        UniquePtr(pointer p) noexcept;
+        explicit UniquePtr(pointer ptr);
 
         // disable copy constructor
         UniquePtr(const UniquePtr &) = delete;
@@ -87,16 +87,161 @@ namespace task {
         pointer operator->() const noexcept;
     };
 
+    /**
+     * SharedPtr is a smart pointer that retains shared ownership of an object through a pointer.
+     * Several SharedPtr objects may own the same object.
+     */
+
     template<class T>
     class SharedPtr {
-        // Your code goes here...
+    public:
+        using pointer = T *;
+        using element_type = T;
+        using weak_type = WeakPtr<T>;
+    private:
+        pointer current_ptr = nullptr;
+        long *control_block = nullptr;
+    public:
+
+        // constructors
+        /**
+         * Constructs a shared_ptr with no managed object, i.e. empty shared_ptr
+         */
+        SharedPtr() noexcept;
+
+        /**
+         * Constructs a shared_ptr with ptr as the pointer to the managed object.
+         */
+        explicit SharedPtr(pointer ptr);
+
+        /**
+         * Constructs a shared_ptr which shares ownership of the object managed by other.
+         * If other manages no object, *this manages no object too.
+         */
+        SharedPtr(const SharedPtr &other) noexcept;
+
+        /**
+         *  Move-constructs a shared_ptr from r. After the construction,
+         *  *this contains a copy of the previous state of r, r is empty and its stored pointer is null.
+         */
+        SharedPtr(const SharedPtr &&r) noexcept;
+
+        /**
+         *  Constructs a shared_ptr which shares ownership of the object managed by r.
+         */
+        explicit SharedPtr(const weak_type &r) noexcept;
+
+
+        /**
+         * If *this owns an object and it is the last shared_ptr owning it, the object is destroyed.
+         */
+        ~SharedPtr();
+
+        // operator=
+
+        /**
+         * Replaces the managed object with the one managed by r.
+         * If *this already owns an object and it is the last shared_ptr owning it, and r is not the same as *this,
+         * the object is destroyed
+         */
+        SharedPtr &operator=(const SharedPtr &r) noexcept;
+
+        SharedPtr &operator=(SharedPtr &&r) noexcept;
+
+
+        // modifiers
+
+        /**
+         * Replaces the managed object with an object pointed to by ptr.
+         * If *this already owns an object and it is the last shared_ptr owning it, the object is destroyed.
+         * If the object pointed to by ptr is already owned, the function generally results in undefined behavior.
+         * @ptr	-	pointer to an object to acquire ownership of.
+         */
+        void reset(pointer ptr);
+
+        /**
+         * Exchanges the contents of *this and r.
+         * @r - smart pointer to exchange the contents with.
+         */
+        void swap(SharedPtr &r) noexcept;
+
+        //observers
+
+        /**
+         * Returns the stored pointer.
+         */
+        pointer get() const noexcept;
+
+        /**
+         * Returns the result of dereferencing the stored pointer, i.e., *get().
+         * The behavior is undefined if the stored pointer is null.
+         */
+        element_type &operator*() const noexcept;
+
+        /**
+         * Returns the stored pointer, i.e., get().
+         */
+        pointer operator->() const noexcept;
+
+        /**
+         * Returns the number of different shared_ptr instances (this included) managing the current object.
+         * If there is no managed object, ​0​ is returned.
+         */
+        long use_count() const noexcept;
+
     };
+
+
+    /**
+     * WeakPtr is a smart pointer that holds a non-owning ("weak") reference
+     * to an object that is managed by SharedPtr. It must be converted to SharedPtr
+     * in order to access the referenced object.
+     */
 
     template<class T>
     class WeakPtr {
-        // Your code goes here...
+    public:
+        using pointer = T *;
+        using element_type = T;
+    private:
+        pointer current_ptr = nullptr;
+    public:
+        // constructors
+
+        WeakPtr() noexcept;
+
+        explicit WeakPtr(const SharedPtr<T> &s) noexcept;
+
+        WeakPtr(const WeakPtr<T> &r) noexcept;
+
+        WeakPtr(const WeakPtr<T> &&r) noexcept;
+
+        ~WeakPtr();
+
+        WeakPtr &operator=(WeakPtr &r);
+
+        WeakPtr &operator=(WeakPtr &&r);
+
+        //observers
+
+        long use_count();
+
+        bool expired() const noexcept;
+
+        SharedPtr<T> lock() const noexcept;
+
+        // modifiers
+
+        void reset() noexcept;
+
+        void swap();
+
     };
 
+    template<class T>
+    WeakPtr<T>::WeakPtr(SharedPtr &s) {
+
+    }
 
 }  // namespace task
 
--- smart_pointers/src/smart_pointers.tpp
+++ smart_pointers/src/smart_pointers.tpp
@@ -5,7 +5,7 @@ namespace task {
     // Definitions
 
     template<class T>
-    UniquePtr<T>::UniquePtr(pointer p) noexcept: current_ptr(p) {}
+    UniquePtr<T>::UniquePtr(pointer ptr): current_ptr(ptr) {}
 
     template<class T>
     UniquePtr<T>::UniquePtr(UniquePtr &&u) noexcept {
@@ -21,15 +21,15 @@ namespace task {
     }
 
     template<class T>
-    UniquePtr<T> &UniquePtr<T>::operator=(UniquePtr &&r) noexcept {
-        reset(r.release());
+    UniquePtr<T> &UniquePtr<T>::operator=(UniquePtr &&right) noexcept {
+        reset(right.release());
         return *this;
     }
 
     template<class T>
     typename UniquePtr<T>::pointer UniquePtr<T>::release() noexcept {
-        pointer old_ptr = get();
-        this->current_ptr = nullptr;
+        pointer old_ptr = current_ptr;
+        current_ptr = nullptr;
         return old_ptr;
     }
 
@@ -61,4 +61,104 @@ namespace task {
     typename UniquePtr<T>::pointer UniquePtr<T>::operator->() const noexcept {
         return get();
     }
+
+
+    template<class T>
+    SharedPtr<T>::SharedPtr(SharedPtr::pointer ptr) : current_ptr(ptr), control_block(new long(1)) {
+    }
+
+    template<class T>
+    SharedPtr<T>::SharedPtr(const SharedPtr &other) noexcept {
+        if (other.current_ptr != nullptr) {
+            if (this != &other) {
+                current_ptr = other.current_ptr;
+                control_block = other.control_block;
+            }
+            *control_block += 1;
+        }
+    }
+
+    template<class T>
+    SharedPtr<T>::SharedPtr(const SharedPtr &&r) noexcept {
+        current_ptr = r.current_ptr;
+        control_block = r.control_block;
+        r.current_ptr = nullptr;
+        r.control_block = nullptr;
+    }
+
+    template<class T>
+    typename SharedPtr<T>::pointer SharedPtr<T>::get() const noexcept {
+        return current_ptr;
+    }
+
+    template<class T>
+    void SharedPtr<T>::reset(SharedPtr::pointer ptr) {
+        // manage old state of *this
+        if (current_ptr != nullptr) {
+            if (*control_block == 1) {
+                delete current_ptr;
+                delete control_block;
+            } else {
+                *control_block -= 1;
+            }
+        }
+        current_ptr = ptr;
+        control_block = new long(1);
+    }
+
+    template<class T>
+    void SharedPtr<T>::swap(SharedPtr &r) noexcept {
+        std::swap(current_ptr, r.current_ptr);
+        std::swap(control_block, r.control_block);
+    }
+
+    template<class T>
+    typename SharedPtr<T>::element_type &SharedPtr<T>::operator*() const noexcept {
+        return *get();
+    }
+
+    template<class T>
+    typename SharedPtr<T>::pointer SharedPtr<T>::operator->() const noexcept {
+        return get();
+    }
+
+    template<class T>
+    long SharedPtr<T>::use_count() const noexcept {
+        if (current_ptr == nullptr) {
+            return 0;
+        } else {
+            return *control_block;
+        }
+    }
+
+    template<class T>
+    SharedPtr<T>::~SharedPtr() {
+        if (current_ptr != nullptr) {
+            if (*control_block == 1) {
+                delete current_ptr;
+                delete control_block;
+            } else {
+                *control_block -= 1;
+            }
+        }
+    }
+
+    template<class T>
+    SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr &r) noexcept {
+        SharedPtr<T>(r).swap(*this);
+        return *this;
+    }
+
+    template<class T>
+    SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr &&r) noexcept {
+        SharedPtr<T>(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    template<class T>
+    SharedPtr<T>::SharedPtr() noexcept = default;
+
+
+
+
 } //task namespace
